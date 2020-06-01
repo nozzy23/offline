243477f3c53c0470b7f30a9d53c7acfdec9038bd
Oscar Nunez <noscar07@gmail.com>
2020-06-01 18:52:38 -0400

    creating  mongodb

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # offline
+
+Application to help add expenses online and offline.
+
+please install the package.json file to continue 
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+
+mongoose.connect(MONGODB_URI);
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
